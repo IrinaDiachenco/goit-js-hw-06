04226925e7766a6274e1305fbb112f86530ad5f0
Irina Diachenco <bilairina@ukr.net>
2020-09-20 21:43:35 +0300

    6 модуль

---
+++ task-1.js
@@ -0,0 +1,11 @@
+const getUserNames = (array) => array.map(({ name }) => name);
+console.log(getUserNames(users));
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
